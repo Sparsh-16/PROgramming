3facb58eec890e5dca720a39d2da2fdc25b379ef
Sparsh-16 <114205171+Sparsh-16@users.noreply.github.com>
2022-11-16 20:32:24 +0530

    Add files via upload
    started learning c language and try to be consisted.

---
+++ learn-programming.c
@@ -0,0 +1,19 @@
+/*
+ Module:	1
+ Topic:		write and explore you first program in c
+ */
+
+
+#include <stdio.h>
+
+int main() {
+
+	//my first print statement while learning c
+	//new line character is "\n".
+
+	printf("Hello Internshala. \n\n");
+	printf("I am learning c language.");
+
+
+	return 0;
+}
