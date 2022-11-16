64248acf445268dd5336d10ca2b02d818776c1df
Sparsh-16 <114205171+Sparsh-16@users.noreply.github.com>
2022-11-16 21:12:24 +0530

    Add files via upload

---
+++ Tokens.cpp
@@ -0,0 +1,27 @@
+/*
+ Topic:	Tokens
+ */
+
+#include<stdio.h>
+
+int main() {
+    char name[15] = "farm house"
+    		// 15 is no. of characters in our literal including space
+	char size = 'M';
+	int personItServes = 2;
+	float mrp = 365.8;
+
+	//CODE CHALLANGE
+
+	char countryName[10] = "India";
+	char money = '$';
+	float growth = 2.2;
+	int year = 2015;
+	// primary data types:char,int,long,float,double
+	// derived data types:array,pointer,string
+	// user defined data types:structure,union,enum
+
+	 printf("%ld", sizeof(double))
+	return 0;
+
+}
